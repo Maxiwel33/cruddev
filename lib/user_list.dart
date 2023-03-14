6a256418bcb90098a800a24c7703a60dd4a08e37
Maxiwel <cladelphi007@gmail.com>
2023-03-14 19:13:57 -0300

    segundo commit

--- lib/user_list.dart
+++ lib/user_list.dart
@@ -1,17 +1,18 @@
 import 'package:cruddev/componentes/user_tile.dart';
-import 'package:cruddev/data/dummy_user.dart';
 import 'package:cruddev/models/users.dart';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class UserList extends StatelessWidget {
   const UserList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Map<String, User> user = {...DUMMY_USERS};
+    Map<String, User> user = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
         title: const Text(
           'Lista de Usuarios',
           textAlign: TextAlign.center,
